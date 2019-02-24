21bd88e9326b515e9a107753756f6bccdb49c6e0
miladvafaieenezhad <miladvafaieenezhad@gmail.com>
2019-02-24 03:10:32 -0500

    my project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # title of project:guess sign game
 ### date and time at which make was run:
-Sat Feb 23 15:00:31 EST 2019
+Sun Feb 24 03:08:49 EST 2019
 ### number of lines:
-26
+30
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+function file_number {
+ls -l | wc -l
+}
 echo "how many files are in the current directory?:"
 read response
 echo "you guess: $response"
-while [[ $response -ne 5 ]]
+while [[ $response -ne file_number ]]
 do
 function high {
 echo "sorry, $response is too high"
@@ -9,13 +13,13 @@ echo "sorry, $response is too high"
 function low {
 echo "sorry, $response is too low"
 }
-if [[ $response -gt 5 ]]
+if [[ $response -gt file_number ]]
 then
 high
 echo "try again:"
 read response
 echo "your new guess: $response"
-elif [[ $response -lt 5 ]]
+elif [[ $response -lt file_number ]]
 then
 low
 echo "try again:"
