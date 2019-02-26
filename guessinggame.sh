e164b2e989915e9ca0cdb94677d245edea223ea3
miladvafaieenezhad <miladvafaieenezhad@gmail.com>
2019-02-26 08:46:10 -0500

    my project

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,30 +1,45 @@
 #!/usr/bin/env bash
-function file_number {
-ls -l | wc -l
-}
-echo "how many files are in the current directory?:"
-read response
-echo "you guess: $response"
-while [[ $response -ne file_number ]]
+function forbiden {
+while [[ $response =~ ^[a-zA-Z*/_%-+@$\s]*$  ]]
 do
+  echo "you have to enter a  number"
+  echo "try again:"
+  read response
+  echo "your new guess: $response"
+done
+}
+function file_number {
+echo $(ls -l | wc -l)
+}
+x=$(file_number)
 function high {
 echo "sorry, $response is too high"
 }
 function low {
 echo "sorry, $response is too low"
 }
-if [[ $response -gt file_number ]]
-then
-high
-echo "try again:"
+
+
+
+echo "how many files are in the current directory?:"
 read response
-echo "your new guess: $response"
-elif [[ $response -lt file_number ]]
-then
-low
-echo "try again:"
-read response
-echo "your new guess: $response"
-fi
+echo "you guess: $response"
+while [[ $response -ne x ]]
+  do
+  forbiden
+      if [[ $response -gt x ]]
+      then
+      high
+      echo "try again:"
+      read response
+      echo "your new guess: $response"
+      elif [[ $response -lt x ]]
+      then
+      low
+      echo "try again:"
+      read response
+      echo "your new guess: $response"
+      fi
 done
 echo "~*~*~congratulation~*~*~"
+
