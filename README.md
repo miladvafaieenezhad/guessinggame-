461c16589d87b5814b0ba404cfb04e0e4c618138
miladvafaieenezhad <miladvafaieenezhad@gmail.com>
2019-02-27 03:50:32 -0500

    my project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # title of project:guess sign game
 ### date and time at which make was run:
-Tue Feb 26 08:52:50 EST 2019
+Wed Feb 27 03:44:07 EST 2019
 ### number of lines:
-42
+45
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
-function forbiden {
-while [[ $response =~ ^[a-zA-Z*/_%-+@$\s]*$  ]]
+function forbidden {
+while [[ ! $response =~ ^[0-9]+$ ]]  
 do
-  echo "you have to enter a  number"
+  echo "please enter a natural number"
   echo "try again:"
   read response
   echo "your new guess: $response"
 done
 }
-function file_number {
-echo $(ls -l | wc -l)
-}
-x=$(file_number)
+
+for m in $(ls -al) #consider hidden files
+do
+ let count=$count+1
+ done
+let count=$count-1  #-1 for total, which is not file.
+let x=$count
 function high {
 echo "sorry, $response is too high"
 }
@@ -21,16 +24,16 @@ echo "sorry, $response is too low"
 echo "how many files are in the current directory?:"
 read response
 echo "you guess: $response"
-while [[ $response -ne x ]]
+while [[ $response -ne $x ]]
   do
-  forbiden
-      if [[ $response -gt x ]]
+  forbidden
+      if [[ $response -gt $x ]]
       then
       high
       echo "try again:"
       read response
       echo "your new guess: $response"
-      elif [[ $response -lt x ]]
+      elif [[ $response -lt $x ]]
       then
       low
       echo "try again:"
