1cb639e474ef0e0e07604e419ffe415445fbae08
miladvafaieenezhad <miladvafaieenezhad@gmail.com>
2019-03-01 03:10:23 -0500

    my project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # title of project:guess sign game
 ### date and time at which make was run:
-Thu Feb 28 14:54:32 EST 2019
+Fri Mar  1 03:09:38 EST 2019
 ### number of lines:
-35
+38
--- guessinggame.sh
+++ guessinggame.sh
@@ -8,28 +8,31 @@ echo "sorry, $response is too low"
 }
 echo "how many files are in the current directory?:"
 read response
-echo "you guess: $response"
-while [[ ! $response =~ ^[0-9]+$ ]]
-  do
-  echo "please enter a natural number"
-  echo "try again:"
-  read response
-  echo "your new guess: $response"
-while [[ $response =~ ^[0-9]+$  ]]
- do
+echo "your guess: $response"
+while [[  $response != $x ]]
+do
+   if [[ ! $response =~ ^[0-9]+$ ]]
+     then
+     echo "please enter a natural number"
+     echo "try again:"
+     read response
+     echo "your new guess: $response"
+   fi
+   if [[ $response =~ ^[0-9]+$  ]]
+      then
       if [[ $response -gt $x ]]
-      then
-      high
-      echo "try again:"
-      read response
-      echo "your new guess: $response"
-      elif [[ $response -lt $x ]]
-      then
-      low
-      echo "try again:"
-      read response
-      echo "your new guess: $response"
+         then
+         high
+         echo "try again:"
+         read response
+         echo "your new guess: $response"
+         elif [[ $response -lt $x ]]
+         then
+         low
+         echo "try again:"
+         read response
+         echo "your new guess: $response"
       fi
-done
+   fi
 done
 echo "~*~*~congratulation~*~*~"
